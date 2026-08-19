754c965a8b9bb4de06f7473f01282614fd224ce7
Igoradvance <igoro.advance@gmail.com>
2026-08-19 10:15:29 +0300

    chore: bump to Build 67

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,5 +1,5 @@
 ﻿export const VERSION = '1.0.0'
-export const BUILD = 66
+export const BUILD = 67
 export const BUILD_DATE = '2026-08-19'
 export const APP_NAME = 'TP Reject – מערכת ניהול תקלות'
 export const COPYRIGHT = '© 2026 Igor Ositchansky – Advance Engineering. כל הזכויות שמורות.'
